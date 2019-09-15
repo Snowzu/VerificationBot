d176708aa5f1f041f445e595bd802bd7ea8802c7
Snowz <46151873+Snowzok@users.noreply.github.com>
2019-09-15 16:42:50 +1200

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='YOUR_PREFIX_HERE')
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}.\n-----------')
+    
+
+@client.event
+async def on_member_join(member):
+    unverified = discord.utils.get(member.guild.roles, name="UNVERIFIED_ROLE_NAME_HERE")
+    await member.add_roles(unverified)
+    
+
+def is_channel(ctx):
+    return ctx.channel.id == VERIFICATION_CHANNEL_ID
+
+
+@client.command()
+@commands.check(is_channel)
+async def verify(ctx):
+    unverified = discord.utils.get(ctx.guild.roles, name="UNVERIFIED_ROLE_NAME_HERE")
+    if unverified in ctx.author.roles:
+        verify = discord.utils.get(ctx.guild.roles, name="VERIFIED_ROLE_NAME_HERE")
+        msg = await ctx.send('Verification has been sent in DMs')
+        await msg.add_reaction('✅')
+        e = discord.Embed(color=0x7289da)
+        e.add_field(name='Please complete the captcha below to gain access to the server.',
+                    value='**NOTE:** This is **Case and Space Sensitive**')
+        e.set_image(url='https://cdn.discordapp.com/attachments/558818895868198913/605614132959379471/captcha.png')
+        await ctx.author.send(embed=e)
+
+        def check(m):
+            return m.content == 'aHr vE'
+
+        msg = await client.wait_for('message', check=check)
+        e = discord.Embed(color=0x7289da)
+        await ctx.author.remove_roles(unverified)
+        e.add_field(name='Thank you for verifying!', value='You now have access to the server.')
+        await ctx.author.send(embed=e)
+        await ctx.author.add_roles(verify)
+    else:
+        await ctx.send('You are already verified!')
+        
+client.run('YOUR_TOKEN_HERE')
